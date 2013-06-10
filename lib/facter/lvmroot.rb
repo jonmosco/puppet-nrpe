cc7a99b62408471b13be3e25c547cb6f41b41bf4
Jon Mosco <jonny.mosco@gmail.com>
2013-06-10 15:33:51 -0400

    Update quoting in fact

--- lib/facter/lvmroot.rb
+++ lib/facter/lvmroot.rb
@@ -3,12 +3,13 @@
 # First custom fact
 
 Facter.add("root_partition") do
-  confine :kernel => "Linux"
+  #confine :kernel => "Linux"
+  confine :kernel => :linux
   setcode do
     # problem: if the partion was created with kickstart, it will not have a 
     # name associated with it, just a UUID.  If this is the case, get our 
     # partion name from df -h ?
-    #Facter::Util::Resolution.exec('/bin/df -h | sed -n 2p')
-    Facter::Util::Resolution.exec("grep '/ ' /etc/fstab | sed 's/\ .*//")
+    Facter::Util::Resolution.exec('/bin/df -h | sed -n 2p')
+    #Facter::Util::Resolution.exec('grep "/ " /etc/fstab | sed "s/\ .*//"'')
   end
 end
